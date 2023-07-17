3f8fa6411660603f9397232e09309c747b808dec
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-07-18 00:54:43 +0900

    feat: add provider

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:hana_hackerton/pages/PurchaseCompletePage.dart';
+import 'package:hana_hackerton/pages/PurchasePage.dart';
 import 'package:hana_hackerton/pages/SearchPage.dart';
 import 'package:hana_hackerton/pages/MainPage.dart';
+import 'package:hana_hackerton/provider/RouteProvider.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
@@ -10,24 +14,29 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 1;
+  late RouteProvider _routeProvider;
 
   final List<Widget> _pages = [
     SearchPage(),
     MainPage(),
+    PurchasePage(),
+    PurchaseCompletePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
+    _routeProvider = Provider.of<RouteProvider>(context);
+
     return Scaffold(
-      body: _pages[_selectedIndex],
+      body: _pages[Provider.of<RouteProvider>(context).currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xff419290), // Change the icon color here
+        currentIndex: Provider.of<RouteProvider>(context).currentIndex >= _pages.length
+            ? Provider.of<RouteProvider>(context).currentIndex
+            : 1,
+        selectedItemColor: const Color(0xff419290),
+        // Change the icon color here
         onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
+          _routeProvider.setIndex(index);
         },
         items: [
           BottomNavigationBarItem(
