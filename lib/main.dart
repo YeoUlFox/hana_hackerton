1f99434f3ed4fdb182cd3a4d7dc971729bf4b516
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-07-17 16:26:39 +0900

    feat: add styles

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,36 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const ColorScheme myCustomColorScheme = ColorScheme(
+      primary: Color(0xFF20C997),         // main
+      primaryVariant: Color(0xFF186D63),  // M5
+      secondary: Color(0xFF0CA678),       // M4
+      secondaryVariant: Color(0xFF63E6BE),// M2
+      surface: Color(0xFFC3FAE8),         // M1
+      background: Color(0xFFFAFBFB),      // background
+      onPrimary: Color(0xFF212529),       // N7
+      onSecondary: Color(0xFF868E96),     // N6
+      onSurface: Color(0xFFCED4DA),       // N4
+      onBackground: Color(0xFFE9ECEF),    // N3
+      onError: Color(0xFFFFFFFF),         // WHITE
+      outline: Color(0xFF7B61FF),         // highlight
+      brightness: Brightness.light, // 밝은 테마
+    );
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hana Reborn',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 48, fontWeight: FontWeight.bold), // h1
+          headline2: TextStyle(fontSize: 35, fontWeight: FontWeight.bold), // h2
+          headline3: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), // h3
+          headline4: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // h4
+          headline5: TextStyle(fontSize: 14, fontWeight: FontWeight.bold), // h5
+          bodyText1: TextStyle(fontSize: 16),
+          bodyText2: TextStyle(fontSize: 14),
+        ),
         useMaterial3: true,
       ),
       home: const MainPage(),
